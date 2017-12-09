7880e2d2a556139b62cceeb3bc15b13c3e542f04
Dimitriy Kosianchuk <kosianchuk.d@gmail.com>
2017-12-09 10:40:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var keyboard = [
+    ['q', 'w', 'e'],
+    ['a', 's', 'd'],
+    ['z', 'x', 'c']
+]
+
+function Keyboard() {
+
+};
+
+function Key(value) {
+    this.value = value;
+}
+
+window.onload = function() {
+
+}
