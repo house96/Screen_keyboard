45f0939b3f6f20f17d471d90947fed063c84a02f
Dimitriy Kosianchuk <kosianchuk.d@gmail.com>
2017-12-09 22:43:43 +0200

    add keyboard

--- main.js
+++ main.js
@@ -1,17 +1,65 @@
-var keyboard = [
-    ['q', 'w', 'e'],
-    ['a', 's', 'd'],
-    ['z', 'x', 'c']
-]
+Keyboard.model =[];
 
 function Keyboard() {
 
 };
 
-function Key(value) {
-    this.value = value;
+Keyboard.keyRender = function(target, keys) {
+    var row;
+    for (var i = 0; i < keys.length; i++) {
+        this.model.push([]);
+        row = document.createElement('div');
+        row.classList.add('keyboard__row');
+        target.appendChild(row);
+
+        for (var j = 0; j < keys[i].length; j++) {
+            this.model[i].push(new Key({
+                value: keys[i][j],
+                callback: function() {},
+                target: row
+            }));
+    }
+}
 }
 
-window.onload = function() {
+Keyboard.render = function(target, keys=[]) {
+    if (target === undefined) {
+        return;
+    }
+    var container = document.createElement('div');
+    container.classList.add('keyboard__container');
+    Keyboard.keyRender(container, keys);
+
+        target.appendChild(container);
 
 }
+function Key(options) {
+    if(options.callback === undefined) {
+        callback = function() {};
+    }
+    this.value = options.value;
+    this.callback = options.callback;
+    this.render = function() {
+        var key = document.createElement('div');
+
+        key.classList.add('keyboard__key');
+        key.innerHTML = this.value;
+        key.addEventListener('click', this.callback);
+
+        options.target.appendChild(key);
+    };
+    this.render();
+}
+
+window.onload = function () {
+    var keyboardData = [
+        ['q', 'w', 'e'],
+        ['a', 's', 'd'],
+        ['z', 'x', 'c']
+    ];
+
+    var keyboard = document.querySelector('#keyboard');
+    Keyboard.render(keyboard, keyboardData);
+
+    
+    }
