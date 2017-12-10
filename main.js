1c90ed65e1a7dd28594251a72a34abcec9b7a686
Dimitriy Kosianchuk <kosianchuk.d@gmail.com>
2017-12-10 23:48:04 +0200

    add caps

--- main.js
+++ main.js
@@ -1,28 +1,64 @@
-Keyboard.model =[];
-
 function Keyboard() {
+    var _model = [];
 
+    this.getModel = function () {
+        return _model;
+    }
+
+    this.setModel = function (value) {
+        _model = value;
+    }
+
+    this.pushModel = function (value) {
+        _model.push(value);
+
+        return _model;
+    }
+    this.tabButton = function () {
+
+    }
+
+    this.capsButton = function () {
+
+        for (var i = 0; i < _model.length; i++) {
+            for (var j = 0; j < _model[i].length; j++) {
+                _model[i][j].value = _model[i][j].value.toUpperCase();
+
+            }
+        }
+    }
+    this.updateValue = function () {
+        for (var i = 0; i < _model.length; i++) {
+            for (var j = 0; j < _model[i].length; j++) {
+                _model[i][j].element.innerHTML = _model[i][j].value;
+
+            }
+        }
+    }
 };
 
-Keyboard.keyRender = function(target, keys) {
+Keyboard.keyRender = function (target, keys) {
     var row;
     for (var i = 0; i < keys.length; i++) {
-        this.model.push([]);
+        new Key().pushModel([]);
         row = document.createElement('div');
         row.classList.add('keyboard__row');
         target.appendChild(row);
 
         for (var j = 0; j < keys[i].length; j++) {
-            this.model[i].push(new Key({
-                value: keys[i][j],
-                callback: function() {},
-                target: row
+            var model = new Key().getModel();
+            model[i].push(new Key({
+                value: keys[i][j].value,
+                callback: keys[i][j].callback || keys.callbacks.input,
+                target: row,
+                element: null
             }));
+            new Key().setModel(model);
+        }
     }
 }
-}
-
-Keyboard.render = function(target, keys=[]) {
+Key.prototype = new Keyboard();
+Keyboard.render = function (target, keys = []) {
     if (target === undefined) {
         return;
     }
@@ -30,36 +66,78 @@ Keyboard.render = function(target, keys=[]) {
     container.classList.add('keyboard__container');
     Keyboard.keyRender(container, keys);
 
-        target.appendChild(container);
+    target.appendChild(container);
 
 }
-function Key(options) {
-    if(options.callback === undefined) {
-        callback = function() {};
+
+function Key(options = {}) {
+    if (options.callback === undefined) {
+        options.callback = function () {};
     }
     this.value = options.value;
     this.callback = options.callback;
-    this.render = function() {
-        var key = document.createElement('div');
+    this.render = function () {
+        options.element = document.createElement('div');
 
-        key.classList.add('keyboard__key');
-        key.innerHTML = this.value;
-        key.addEventListener('click', this.callback);
+        options.element.classList.add('keyboard__key');
+        options.element.innerHTML = this.value;
+        options.element.addEventListener('click', this.callback);
+        this.element = options.element;
+        if (options.target) {
+            options.target.appendChild(options.element);
+        }
+    }
 
-        options.target.appendChild(key);
-    };
     this.render();
 }
 
 window.onload = function () {
+    var callbacks = {
+        caps: function () {
+            new Key().capsButton();
+            new Key().updateValue();
+        },
+        tab: function () {
+            console.log(this, 'tab')
+        },
+        input: function () {
+            console.log(this, 'input');
+        }
+    }
     var keyboardData = [
-        ['q', 'w', 'e'],
-        ['a', 's', 'd'],
-        ['z', 'x', 'c']
+        [{
+            value: 'Tab',
+            callback: callbacks.tab
+        }, {
+            value: 'q'
+        }, {
+            value: 'w'
+        }, {
+            value: 'e'
+        }],
+        [{
+            value: 'Caps',
+            callback: callbacks.caps
+        }, {
+            value: 'a'
+        }, {
+            value: 's'
+        }, {
+            value: 'd'
+        }],
+        [{
+            value: 'Shft'
+        }, {
+            value: 'z'
+        }, {
+            value: 'x'
+        }, {
+            value: 'c'
+        }]
     ];
 
+    keyboardData.callbacks = callbacks;
     var keyboard = document.querySelector('#keyboard');
     Keyboard.render(keyboard, keyboardData);
 
-    
-    }
+}
